5dcef6b8cb62dfa95b5de05c27192f9334921efd
Sune Kirkeby <mig@ibofobi.dk>
2013-03-23 17:46:27 +0100

    Absolute URLs in production.
    This fixes the 404 page.

--- publishconf.py
+++ publishconf.py
@@ -9,10 +9,9 @@ SITEURL = 'http://ibofobi.dk'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
+# The 404-page does not work with relative URLs, since it does not have any
+# one home, so use absolute URLs in production.
+RELATIVE_URLS = False
 
 #DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "UA-3678459-1"
